815490af1f535e52aed0461e4000bd1e704bbccf
Neil Quispe <neil.quispe@irdigital.pe>
2020-07-13 21:13:40 -0500

    arquetipo de micro service task

---
+++ src/main/java/org/example/microservicetask/MicroServiceTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.microservicetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceTaskApplication.class, args);
+	}
+
+}
